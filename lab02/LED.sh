55f012110dcd67c69a99b1619dbbcd0149f126d4
Karina <karina.catuna@hotmail.com>
2024-10-16 22:42:54 -0300

    Alteracao

--- lab02/LED.sh
+++ lab02/LED.sh
@@ -2,44 +2,142 @@
 
 # script baseado no código disponibilizado em:
 # Derek Molloy, Exploring Raspberry Pi: Interfacing to the Real World with Embedded Linux,
-# Wiley 2016, ISBN 978-1-119-1868-1, http://www.exploringrpi.com/
+# Wiley 2016, ISBN 978-1-119-1868-1, http://www.exploringrpi.com/ oi
 
-LED_GPIO=16  # Usar uma variavel facilita alteracoes futuras na porta usada
+LED_AMAR=16  # Usar uma variavel facilita alteracoes futuras na porta usada
+LED_VERM=20
+LED_VERD=21
 
-# funcoes Bash
-function setLED
-{                                      # $1 eh o primeiro argumento passado para a funcao
-	echo $1 >> "/sys/class/gpio/gpio$LED_GPIO/value"
-}
+VALUE_VERM="/sys/class/gpio/gpio$LED_VERM/value"
+VALUE_VERD="/sys/class/gpio/gpio$LED_VERD/value"
+VALUE_AMAR="/sys/class/gpio/gpio$LED_AMAR/value"
 
-# Inicio do programa
-if [ $# -ne 1 ]; then                  # se nao houver exatamente um argumento passado ao programa
-	echo "Nenhum comando passado. Uso: ./LED.sh command,"
-	echo "onde comando pode ser: setup, on, off, status e close"
-	echo -e " ex.: ./LED.sh setup, e em seguinda, ./LED.sh on"
-	exit 2                         # erro que indica numero invalido de argumentos
-fi
 
-echo "O comando passado foi: $1"
+# fazendo  o setup dos 3 LEDs
+echo $LED_AMAR >> "/sys/class/gpio/export"
+echo $LED_VERM >> "/sys/class/gpio/export"
+echo $LED_VERD >> "/sys/class/gpio/export"
 
-if [ "$1" == "setup" ]; then
-	echo "Habilitando a GPIO numero $LED_GPIO"
-	echo $LED_GPIO >> "/sys/class/gpio/export"
-	sleep 1                        # esperar 1 segundo para garantir que a gpio foi exportada
-	echo "out" >> "/sys/class/gpio/gpio$LED_GPIO/direction"
-elif [ "$1" == "on" ]; then
-	echo "Acendendo o LED"
-	setLED 1                       # 1 eh recebido como $1 na funcao setLED
-elif [ "$1" == "off" ]; then
-	echo "Desligando o LED"
-	setLED 0                       # 0 eh recebido como $1 na funcao setLED
-elif [ "$1" == "status" ]; then
-	state=$(cat "/sys/class/gpio/gpio$LED_GPIO/value")
-	echo "O estado do LED eh: $state"
-elif [ "$1" == "close" ]; then
-	echo "Desabilitando a GPIO numero $LED_GPIO"
-	echo $LED_GPIO >> "/sys/class/gpio/unexport"
-else
-	echo "Comando nao reconhecido."
-	exit 3                         # erro que indica comando nao reconhecido
-fi
+sleep 0.2
+
+echo "out" >> "/sys/class/gpio/gpio$LED_AMAR/direction"
+echo "out" >> "/sys/class/gpio/gpio$LED_VERM/direction"
+echo "out" >> "/sys/class/gpio/gpio$LED_VERD/direction"
+
+sleep 0.2
+
+# acendendo o vermelho por 2 s
+echo 1 >> $VALUE_VERM
+sleep 2
+echo 0 >> $VALUE_VERM
+
+#acendendo o verde por 1s
+
+echo 1 >> $VALUE_VERD
+sleep 1
+echo 0 >> $VALUE_VERD
+
+#acendendo o amarelo por 1s
+
+echo 1 >> $VALUE_AMAR
+sleep 1
+echo 0 >> $VALUE_AMAR
+
+
+
+# acendendo o vermelho por 2 s
+echo 1 >> $VALUE_VERM
+sleep 2
+echo 0 >> $VALUE_VERM
+
+#acendendo o verde por 1s
+
+echo 1 >> $VALUE_VERD
+sleep 1
+echo 0 >> $VALUE_VERD
+
+#acendendo o amarelo por 1s
+
+echo 1 >> $VALUE_AMAR
+sleep 1
+echo 0 >> $VALUE_AMAR
+
+# acendendo o vermelho por 2 s
+echo 1 >> $VALUE_VERM
+sleep 2
+echo 0 >> $VALUE_VERM
+
+#acendendo o verde por 1s
+
+echo 1 >> $VALUE_VERD
+sleep 1
+echo 0 >> $VALUE_VERD
+
+#acendendo o amarelo por 1s
+
+echo 1 >> $VALUE_AMAR
+sleep 1
+echo 0 >> $VALUE_AMAR
+
+# acendendo o vermelho por 2 s
+echo 1 >> $VALUE_VERM
+sleep 2
+echo 0 >> $VALUE_VERM
+
+#acendendo o verde por 1s
+
+echo 1 >> $VALUE_VERD
+sleep 1
+echo 0 >> $VALUE_VERD
+
+#acendendo o amarelo por 1s
+
+echo 1 >> $VALUE_AMAR
+sleep 1
+echo 0 >> $VALUE_AMAR
+
+# acendendo o vermelho por 2 s
+echo 1 >> $VALUE_VERM
+sleep 2
+echo 0 >> $VALUE_VERM
+
+#acendendo o verde por 1s
+
+echo 1 >> $VALUE_VERD
+sleep 1
+echo 0 >> $VALUE_VERD
+
+#acendendo o amarelo por 1s
+
+echo 1 >> $VALUE_AMAR
+sleep 1
+echo 0 >> $VALUE_AMAR
+
+# acendendo o vermelho por 2 s
+echo 1 >> $VALUE_VERM
+sleep 2
+echo 0 >> $VALUE_VERM
+
+#acendendo o verde por 1s
+
+echo 1 >> $VALUE_VERD
+sleep 1
+echo 0 >> $VALUE_VERD
+
+#acendendo o amarelo por 1s
+
+echo 1 >> $VALUE_AMAR
+sleep 1
+echo 0 >> $VALUE_AMAR
+
+
+
+#FECHAR
+
+
+echo $LED_VERM >> "/sys/class/gpio/unexport"
+echo $LED_VERD >> "/sys/class/gpio/unexport"
+echo $LED_AMAR >> "/sys/class/gpio/unexport"
+
+
+
